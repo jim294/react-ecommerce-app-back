8c19a6f0f60dd5777d0863d7a1249e46bfdb0f3f
jim294 <jeanmariedeschamps25@gmail.com>
2024-06-20 11:59:55 +0200

    update

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const categoryRouter = require('./routes/prodcategoryRoute')
 const blogcategoryRouter = require('./routes/blogCatRoute')
 const brandRouter = require('./routes/brandRoute')
 const colorRouter = require('./routes/colorRoute')
+const enqRouter = require('./routes/enqRoute')
 const couponRouter = require('./routes/couponRoute')
 const cookieParser= require('cookie-parser')
 const morgan = require('morgan')
@@ -32,6 +33,7 @@ app.use('/api/blogcategory', blogcategoryRouter)
 app.use('/api/brand', brandRouter)
 app.use('/api/coupon', couponRouter)
 app.use('/api/color', colorRouter)
+app.use('/api/enquiry', enqRouter)
 
 
 app.use(notFound);
